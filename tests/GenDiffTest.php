d74d085c03a4eda28846dc39675e61436822ba64
harumimax <harumimax@gmail.com>
2019-09-13 19:04:49 +0300

    step 3 add tests

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -10,39 +10,129 @@ Use function \Differ\LibDiffer\toStr;
 
 class GenDiffTest extends TestCase
 {
-    private $diff;
+    //private $diff;
+    private $path;
+    //private $path2;
 
+/*
     protected function setUp(): void
     {
+        if(file_exists($this->path)){
+            rmdir($this->path);
+        }
 
-        $before = '{
+        $temp = sys_get_temp_dir();
+        $this->path1 = $temp . DIRECTORY_SEPARATOR . "file1";
+        $this->path2 = $temp . DIRECTORY_SEPARATOR . "file2";
+
+        $dataBefore = '{
             "host": "hexlet.io",
             "timeout": 50,
             "proxy": "123.234.53.22"
           }';
 
-        $after = '{
+        if (is_writable($this->path1)) {
+            $handle = fopen($this->path1, "ab");
+            if ($handle) {
+                try {
+                    fwrite($handle, $dataBefore);
+                } finally {
+                    fclose($handle);
+                }
+            }
+        }
+        
+        $dataAfter = '{
             "timeout": 20,
             "verbose": true,
             "host": "hexlet.io"
           }';
 
-          $this->diff = new \Differ\GenDiff($before, $after);
+        if (is_writable($this->path2)) {
+            $handle = fopen($this->path2, "ab");
+            if ($handle) {
+                try {
+                    fwrite($handle, $dataAfter);
+                } finally {
+                    fclose($handle);
+                }
+            }
+        }
 
+    }
+*/
+
+    public function testGetDataArray()
+    {
+        if(file_exists($this->path)){
+            rmdir($this->path);
+        }
+        $temp = sys_get_temp_dir();
+        $this->path = $temp . DIRECTORY_SEPARATOR . "file1.json";
+        $dataBefore = '{
+            "host": "hexlet.io",
+            "timeout": 50,
+            "proxy": "123.234.53.22"
+          }';
+        
+        file_put_contents($this->path, $dataBefore);
+
+        $request = getDataArray($this->path);
+        $this->assertIsArray($request);
+        $this->assertFalse(empty($request));
+
+    }
+
+    protected function tearDown(): void 
+    {
+        if(file_exists($this->path)){
+            unlink($this->path);
+        }
+    }
+
+    public function testFindDiff()
+    {
+        $dataBefore = ['timeout' => 50, 'host' => 'hexlet.io'];
+        $dataAfter = ['timeout' => 50, 'host' => 'new.host'];
+        $request = findDiff($dataBefore, $dataAfter);
+
+        $this->assertIsArray($request);
+
+        $this->assertEquals(3, sizeof($request));
+        $this->assertTrue(array_key_exists('timeout', $request));
+        $this->assertTrue(array_key_exists('-host', $request));
+        $this->assertTrue(array_key_exists('+host', $request));
+        $this->assertEquals('timeout', array_search('50', $request));
     }
 
     public function testNotEmpty()
     {
-        $this->assertFalse(empty($this->diff));
+        $data = [
+            'host' => 'hexlet.io',
+            '+timeout' => 20,
+            '-timeout' => 50,
+            '-proxy' => '123.234.53.22',
+            '+verbose' => 1];
+        
+        $this->assertFalse(empty(toStr($data)));
     }
 
     public function testToStr()
     {
-
-        $this->diff->makeDiff();
-        $request = $this->diff->toStr();
+        $data = [
+        'host' => 'hexlet.io',
+        '+timeout' => 20,
+        '-timeout' => 50,
+        '-proxy' => '123.234.53.22',
+        '+verbose' => 1];
+        
+        $request = toStr($data);
+            
+        $this->assertIsString($request);
+        
         $this->assertStringContainsString("hexlet", $request);
         $this->assertStringContainsString("timeout", $request);
         $this->assertStringContainsString("verbose", $request);
+
     }
 }
