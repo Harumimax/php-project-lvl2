53c47b6437b54ac924b50a8761652de4471fac0c
harumimax <harumimax@gmail.com>
2019-10-02 23:21:52 +0300

    fix tests

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -16,14 +16,10 @@ class GenDiffTest extends TestCase
         $beforeFilePath = 'tests/files/before.json';
         $afterFilePath = 'tests/files/after.json';
         $format = 'pretty';
-        $request = genDiff($beforeFilePath, $afterFilePath, $format);
 
-        $this->assertIsString($request);
-        $this->assertFalse(empty($request));
+        $expected = file_get_contents('tests/files/expected1');
 
-        $this->assertStringContainsString("common", $request);
-        $this->assertStringContainsString("setting4", $request);
-        $this->assertStringContainsString("100500", $request);
+        $this->assertEquals($expected, genDiff($beforeFilePath, $afterFilePath, $format));
     }
 
     
@@ -32,15 +28,10 @@ class GenDiffTest extends TestCase
         $beforeFilePath = 'tests/files/before.yaml';
         $afterFilePath = 'tests/files/after.yaml';
         $format = 'pretty';
-        $request = genDiff($beforeFilePath, $afterFilePath, $format);
 
-        $this->assertIsString($request);
-        $this->assertFalse(empty($request));
+        $expected = file_get_contents('tests/files/expected2');
 
-        $this->assertStringContainsString("timeout", $request);
-        $this->assertStringContainsString("host", $request);
-        $this->assertStringContainsString("hexlet.io", $request);
-        $this->assertStringContainsString("proxy", $request);
+        $this->assertEquals($expected, genDiff($beforeFilePath, $afterFilePath, $format));
     }
 
     
@@ -49,15 +40,10 @@ class GenDiffTest extends TestCase
         $beforeFilePath = 'tests/files/before.json';
         $afterFilePath = 'tests/files/after.json';
         $format = 'plain';
-        $request = genDiff($beforeFilePath, $afterFilePath, $format);
 
-        $this->assertIsString($request);
-        $this->assertFalse(empty($request));
+        $expected = file_get_contents('tests/files/expected3');
 
-        $this->assertStringContainsString("Property 'common.setting6' was removed", $request);
-        $this->assertStringContainsString("Property 'common.setting5' was added with value: 'complex value'", $request);
-        $this->assertStringContainsString("Property 'group3' was added with value: 'complex value'", $request);
-        $this->assertStringNotContainsString("setting1", $request);
+        $this->assertEquals($expected, genDiff($beforeFilePath, $afterFilePath, $format));
     }
 
     public function testGenDiffFormatJsonFileJson()
@@ -65,14 +51,9 @@ class GenDiffTest extends TestCase
         $beforeFilePath = 'tests/files/before.json';
         $afterFilePath = 'tests/files/after.json';
         $format = 'json';
-        $request = genDiff($beforeFilePath, $afterFilePath, $format);
 
-        $this->assertIsString($request);
-        $this->assertFalse(empty($request));
+        $expected = file_get_contents('tests/files/expected4');
 
-        $this->assertStringContainsString('{"key":"value"}', $request);
-        $this->assertStringContainsString('"group1":{"+ baz":"bars","- baz":"bas","foo":"bar"}', $request);
-        $this->assertStringContainsString('"+ group3":{"foo":"bar","fee":"100500"}', $request);
+        $this->assertEquals($expected, genDiff($beforeFilePath, $afterFilePath, $format));
     }
-
 }
