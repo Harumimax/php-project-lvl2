342208647cf384fb3108166f1caf955906e21375
harumimax <harumimax@gmail.com>
2019-10-10 01:21:19 +0300

    fix second dublicate

--- src/formatters/toJson.php
+++ src/formatters/toJson.php
@@ -11,51 +11,19 @@ function toSimpleArray(array $diff): array
 {
     $result = [];
     foreach ($diff as $key => $value) {
-        switch ($value['type']) :
-            case "same":
-                $result[$value['key']] = $value['value'];
-                break;
-
-            case "delete":
-                if (is_array($value['value']) && array_key_exists('offset', $value['value'])) {
-                    $value['value'] = deleteOffset($value['value']);
-                }
-                $result["- {$value['key']}"] = $value['value'];
-                break;
-
-            case "new":
-                if (is_array($value['value']) && array_key_exists('offset', $value['value'])) {
-                    $value['value'] = deleteOffset($value['value']);
-                }
-                $result["+ {$value['key']}"] = $value['value'];
-                break;
-
-            case "change":
-                $result["+ {$value['key']}"] = $value['new-value'];
-                $result["- {$value['key']}"] = $value['old-value'];
-                break;
-            
-            case "array":
-                $arrayValue = toSimpleArray($value['value']);
-                $result[$value['key']] = $arrayValue;
-                break;
-        endswitch;
-    }
-
-    /*
-    foreach ($diff as $key => $value) {
         if ($value['type'] == 'same') {
             $result[$value['key']] = $value['value'];
-        } elseif ($value['type'] == 'delete') {
+        } elseif ($value['type'] == 'delete' || $value['type'] == 'new') {
             if (is_array($value['value']) && array_key_exists('offset', $value['value'])) {
-                $value['value'] = deleteOffset($value['value']);
+                $value['value'] = array_filter($value['value'], function ($value, $key) {
+                    return $key != 'offset';
+                }, ARRAY_FILTER_USE_BOTH);
             }
-            $result["- {$value['key']}"] = $value['value'];
-        } elseif ($value['type'] == 'new') {
-            if (is_array($value['value']) && array_key_exists('offset', $value['value'])) {
-                $value['value'] = deleteOffset($value['value']);
+            if ($value['type'] == 'delete') {
+                $result["- {$value['key']}"] = $value['value'];
+            } else {
+                $result["+ {$value['key']}"] = $value['value'];
             }
-            $result["+ {$value['key']}"] = $value['value'];
         } elseif ($value['type'] == 'change') {
             $result["+ {$value['key']}"] = $value['new-value'];
             $result["- {$value['key']}"] = $value['old-value'];
@@ -64,16 +32,6 @@ function toSimpleArray(array $diff): array
             $result[$value['key']] = $arrayValue;
         }
     }
-    */
     
     return $result;
-}
-
-function deleteOffset(array $array): array
-{
-    $valueWithoutOffset = array_filter($array, function ($value, $key) {
-        return $key != 'offset';
-    }, ARRAY_FILTER_USE_BOTH);
-
-    return $valueWithoutOffset;
 }
