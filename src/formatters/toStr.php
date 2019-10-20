432ead27571714f26ec8c1275e9b1351e23786f7
harumimax <harumimax@gmail.com>
2019-10-21 01:21:42 +0300

    fix pretty format

--- src/formatters/toStr.php
+++ src/formatters/toStr.php
@@ -9,39 +9,46 @@ function toStr($diff): string
     return substr(prepareString($diff), 0, -6) . "\n}";
 }
 
+
 function prepareString($diff): string
 {
     $resultArray = array_map(function ($value) {
-        $offset = str_repeat("  ", $value['offset']);
+
+        if ($value['type'] == 'same' || $value['type'] == 'array') {
+            $offset = str_repeat("    ", $value['offset']);
+        } else {
+            $offset = str_repeat("    ", $value['offset'] - 1);
+        }
+            
 
         if ($value['type'] == 'delete' || $value['type'] == 'new') {
             if (is_array($value['value'])) {
-                $offsetForMap = str_repeat("   ", $value['value']['offset']);
+                $offsetForMap = str_repeat("    ", $value['value']['offset']);
                 $arrayValueForStr = array_map(function ($key, $value) use ($offsetForMap, $offset) {
                     if ($key == 'offset') {
                     } else {
-                        return "{$offsetForMap} {$key}: {$value}";
+                        return "{$offsetForMap}{$key}: {$value}";
                     }
                 }, array_keys($value['value']), $value['value']);
 
-                $valueToString = "{\n" . implode("\n", array_diff($arrayValueForStr, array(''))) . "\n{$offset}  }";
+                $valueToString = "{\n" . implode("\n", array_diff($arrayValueForStr, array(''))) . "\n{$offset}    }";
             } else {
                 $valueToString = $value['value'];
             }
         }
 
         if ($value['type'] == 'same') {
-            return "{$offset}  {$value['key']}: {$value['value']}\n";
+            return "{$offset}{$value['key']}: {$value['value']}\n";
         } elseif ($value['type'] == 'delete') {
-            return "{$offset}- {$value['key']}: {$valueToString}\n";
+            return "{$offset}  - {$value['key']}: {$valueToString}\n";
         } elseif ($value['type'] == 'new') {
-            return "{$offset}+ {$value['key']}: {$valueToString}\n";
+            return "{$offset}  + {$value['key']}: {$valueToString}\n";
         } elseif ($value['type'] == 'change') {
-            return ["{$offset}+ {$value['key']}: {$value['new-value']}\n",
-                    "{$offset}- {$value['key']}: {$value['old-value']}\n"];
+            return ["{$offset}  + {$value['key']}: {$value['new-value']}\n",
+                    "{$offset}  - {$value['key']}: {$value['old-value']}\n"];
         } elseif ($value['type'] == 'array') {
             $arrayValue = prepareString($value['value']);
-            return "{$offset}  {$value['key']}: {$arrayValue}\n";
+            return "{$offset}{$value['key']}: {$arrayValue}\n";
         }
     }, $diff);
 
